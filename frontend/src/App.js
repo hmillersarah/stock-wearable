3b061c82e63df3679c66e64bf20f5fd0734caa3d
smiller342 <smiller342@gatech.edu>
2022-10-22 11:40:54 -0400

    connect frontend + backend for login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
 import axios from 'axios';
 import React, { useState } from 'react';
-import Login from './Login'
+// import Login from './Login'
+import { BrowserRoute, BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './components/Login'
+import Profile from './components/Profile'
+import Header from './components/Header'
+import useToken from './components/useToken'
+import './App.css'
 
 function App() {
 
+  const { token, removeToken, setToken } = useToken();
   const [stockData, setStockData] = useState(null);
 
   function getData() {
@@ -34,19 +40,33 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <BrowserRouter>
+      <div className="App">
 
-        <p>To get your stock details: </p><button onClick={getData}>Click me</button>
-        {stockData && <div>
-          <p>Stock name: {stockData.stock_name}</p>
-          <p>Stock day high: {stockData.dayHigh}</p>
-          <p>Stock past 1 month max close: {stockData.maxClose}</p>
-        </div>}
+        <Header token={removeToken}/>
+        {!token && token!=="" &&token!== undefined?  
+        <Login setToken={setToken} />
+        :(
+          <>
+            <Routes>
+              <Route exact path="/profile" element={<Profile token={token} setToken={setToken}/>}></Route>
+            </Routes>
+          </>
+        )}
 
-      </header>
-    </div>
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+
+          <p>To get your stock details: </p><button onClick={getData}>Click me</button>
+          {stockData && <div>
+            <p>Stock name: {stockData.stock_name}</p>
+            <p>Stock day high: {stockData.dayHigh}</p>
+            <p>Stock past 1 month max close: {stockData.maxClose}</p>
+          </div>}
+
+        </header> */}
+      </div>
+    </BrowserRouter>
   );
 }
 
